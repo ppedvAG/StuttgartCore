f11659b2e9a5a91bba64b7158034f9755d7a51f6
Hannes Preishuber <hannesp@ppedv.de>
2019-03-11 16:34:03 +0100

    Partial PageHandler Ajax

---
+++ StuttgartCore/Pages/modul04/NutzePartial.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StuttgartCore.Pages.modul04
+{
+    public class NutzePartialModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public PartialViewResult OnPost()
+        {
+
+            return Partial("_Zeit");
+        }
+        public PartialViewResult OnGetLade()
+        {
+
+            return Partial("_Zeit");
+        }
+    }
+}
