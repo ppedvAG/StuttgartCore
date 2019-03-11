0797c7adba15c86832dad1e563d24aea06aa8bd8
Hannes Preishuber <hannesp@ppedv.de>
2019-03-11 15:42:48 +0100

    razor page calc post FromForm Attribut

---
+++ StuttgartCore/Pages/modul03/calc2.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StuttgartCore.Pages.modul03
+{
+    public class calc2Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnPostMinus([FromForm] int eins, [FromForm] int zwei)
+        {
+           
+                Ergebnis = eins - zwei;
+            
+
+
+        }
+        public void OnPostPlus([FromForm] int eins, [FromForm] int zwei)
+        {
+          
+                Ergebnis = eins + zwei;
+          
+        }
+    }
+}
