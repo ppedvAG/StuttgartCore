36ec87a27a9baa4de8eb8f293e30bd3e250d9b79
Hannes Preishuber <hannesp@ppedv.de>
2019-03-11 14:46:57 +0100

    Razor Code Samples

---
+++ StuttgartCore/Pages/modul03/hannesfat.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StuttgartCore.Pages.modul02;
+
+namespace StuttgartCore.Pages.modul03
+{
+   
+    public class hannesfatModel : PageModel
+    {
+        public int Feld1 { get; set; }
+        public HannesKlasse MyProperty { get; set; }
+        public void OnGet([FromServices] HannesKlasse hk)
+        {
+            hk.HannesProperty++;
+            Feld1 = 10;
+        }
+    }
+}
