e4f65f4af883e1d7b1a88da3d76bfab3a7d2e379
Hannes Preishuber <hannesp@ppedv.de>
2019-03-11 14:57:27 +0100

    mini calc Razor Page

---
+++ StuttgartCore/Pages/modul03/calc.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StuttgartCore.Pages.modul03
+{
+    public class calcModel : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnGet()
+        {
+            int eins=Int32.Parse(   HttpContext.Request.Query["eins"]);
+            int zwei = Int32.Parse(HttpContext.Request.Query["zwei"]);
+            Ergebnis = eins + zwei;
+        }
+    }
+}
