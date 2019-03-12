b6f615f759b085ca2173c933a6d27a645377d7dd
Hannes Preishuber <hannesp@ppedv.de>
2019-03-12 12:29:07 +0100

    Datei Upload

--- StuttgartCore/Pages/modul05/DateiUpload.cshtml.cs
+++ StuttgartCore/Pages/modul05/DateiUpload.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,6 +13,17 @@ namespace StuttgartCore.Pages.modul05
     {
         public void OnGet()
         {
+
+        }
+        public void OnPost(IFormFile datei)
+        {
+            var pfad = @"C:\aspnetcore\training\StuttgartCore\StuttgartCore\wwwroot\app_data";
+            pfad = Path.Combine(pfad, Path.GetFileName(datei.FileName));
+            using (var fs = new FileStream(pfad, FileMode.Create))
+            {
+                datei.CopyTo(fs);
+            }
+
 
         }
     }
