0dfa36ac8dc1fd19f0932abec56962edf50503e0
Hannes Preishuber <hannesp@ppedv.de>
2019-03-12 15:09:54 +0100

    Connectionstring Setting

---
+++ StuttgartCore/Models/RechnungContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuttgartCore.Models
+{
+    public class RechnungContext:DbContext
+    {
+
+    }
+}
---
+++ StuttgartCore/Models/Rechnungen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuttgartCore.Models
+{
+    public class Rechnungen
+    {
+        public Rechnungen()
+        {
+
+        }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public int KundenID { get; set; }
+        public DateTime Datum { get; set; }
+        public string KopfText { get; set; }
+        public float Summe { get; set; }
+    }
+}
