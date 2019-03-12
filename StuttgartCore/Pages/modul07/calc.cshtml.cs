adf97638530e635352f57afa54fc985bd2fcdeae
Hannes Preishuber <hannesp@ppedv.de>
2019-03-12 16:49:33 +0100

    komplexes Objekt Model Binding Bindproperty

---
+++ StuttgartCore/Pages/modul07/calc.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StuttgartCore.Pages.modul07
+{
+    public class calcModel : PageModel
+    {
+        public int Ergebnis { get; set; }
+        [BindProperty]
+        public rechner myrechner { get; set; }
+        public void OnPostMinus()
+        {
+
+            Ergebnis = myrechner.eins - myrechner.zwei;
+
+
+
+        }
+        public void OnPostPlus()
+        {
+
+            Ergebnis = myrechner.eins + myrechner.zwei;
+
+        }
+    }
+    public class rechner
+    {
+        public int eins { get; set; }
+      
+        public int zwei { get; set; }
+    }
+    
+}
