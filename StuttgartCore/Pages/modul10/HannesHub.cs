486c777027afde6c3aeaef5e9541e777a34255d8
Hannes Preishuber <hannesp@ppedv.de>
2019-03-13 15:39:23 +0100

    Signalr Client Libs und Code

---
+++ StuttgartCore/Pages/modul10/HannesHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuttgartCore.Pages.modul10
+{
+    public class HannesHub:Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.SendAsync("ClientRec", message);
+        }
+    }
+}
