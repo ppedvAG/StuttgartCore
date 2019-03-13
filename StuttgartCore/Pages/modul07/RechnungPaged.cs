e6dd84413150237f16bc58bc58760d1fded5ce54
Hannes Preishuber <hannesp@ppedv.de>
2019-03-13 11:25:51 +0100

    ViewCOmponent Paged Rechnungen

---
+++ StuttgartCore/Pages/modul07/RechnungPaged.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StuttgartCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuttgartCore.Pages.modul07
+{
+    public class RechnungPaged:ViewComponent
+    {
+        private RechnungContext _db;
+        public RechnungPaged(RechnungContext db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke(int page=0)
+        {
+            List<Rechnungen> model = _db.Rechnungens.Skip(page * 3).Take(3).ToList();
+            return View(model); //default.cshtml
+        }
+    }
+}
---
+++ StuttgartCore/Pages/modul07/RechnungenPaging.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StuttgartCore.Pages.modul07
+{
+    public class RechnungenPagingModel : PageModel
+    {
+        public int Seite { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
